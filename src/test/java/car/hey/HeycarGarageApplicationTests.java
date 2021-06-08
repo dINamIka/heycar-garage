30ffa41d570553281b4417e47a2f71a06d916fab
Yevhen Yarmak <yevhen.yarmak@aval.ua>
2021-06-08 23:16:49 +0300

    initial commit

---
+++ src/test/java/car/hey/HeycarGarageApplicationTests.java
@@ -0,0 +1,13 @@
+package car.hey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeycarGarageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
